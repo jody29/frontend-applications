ac021b91819f253f053dbcf1a2442de3eea4e79b
Jody Lorist <66092262+jody29@users.noreply.github.com>
2021-11-26 13:45:48 +0100

    bug

--- src/components/vaccinData.jsx
+++ src/components/vaccinData.jsx
@@ -1,26 +1,13 @@
 import { useContext } from "react"
-import * as topojson from 'topojson-client'
 import VaccinContext from "../providers/VaccinContext"
 
 const Data = () => {
     const json = useContext(VaccinContext)
 
-    const vaccinData = json ? json.covid : 'loading..'
+    const vaccinData = json ? json.adult : 'loading..'
     const townGeo = json ? json.town : 'loading..'
 
-    const gmGeo = topojson.feature(
-        townGeo,
-        townGeo.objects.gm_features
-    ).features
-
-    // const data = json ? json.reduce((arr, obj) => {
-    //     arr.push({ percentage: obj.Vaccination_coverage_completed, region: obj.Region_name })
-    //     return arr
-    // }, []) : 'Loading...'
-
-    // const data = json ? console.log(json.covid, json.town) : null
-
-    console.log(gmGeo)
+    console.log(vaccinData, townGeo)
 
     return <h1>Map incoming..</h1>
 }
--- src/providers/VaccinContext.jsx
+++ src/providers/VaccinContext.jsx
@@ -1,13 +1,42 @@
 import { createContext, useEffect, useState } from "react";
 import covidData from '../data/covid.json'
 import townData from '../data/gemeenten.json'
+import * as topojson from 'topojson-client'
 
 const VaccinContext = createContext(null)
 
 export const VaccinProvider = ({ children }) => {
     const [data, setData] = useState(null)
     useEffect(() => {
-        setData({ covid: covidData, town: townData })
+        setData({  
+            town: topojson.feature(townData, townData.objects.gm_features).features,
+
+            twelve: covidData.reduce((arr, obj) => {
+                if (obj.Age_group === '12+') {
+                    arr.push({
+                        percentage: obj.Vaccination_coverage_completed,
+                        region: obj.Region_code
+                    })
+                } 
+                return arr 
+            }, []),
+
+            teen: covidData.reduce((arr, obj) => {
+                if (obj.Age_group === '12-17') return arr.push({
+                    percentage: obj.Vaccination_coverage_completed,
+                    region: obj.Region_code
+                })
+                return arr 
+            }, []),
+            
+            adult: covidData.reduce((arr, obj) => {
+                if (obj.Age_group === '18+') return arr.push({
+                    percentage: obj.Vaccination_coverage_completed,
+                    region: obj.Region_code
+                })
+                return arr 
+            }, [])
+        })
     }, [])
 
     return (
