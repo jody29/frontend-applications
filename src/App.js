10c648a4f5355728f3e6107ac4586aab9fc4a6f4
Jody Lorist <66092262+jody29@users.noreply.github.com>
2021-11-29 17:37:27 +0100

    Update: added title to the visualisation

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Towns } from './components/map'
 function App() {
   return (
       <div className="App">
+        <h1>Vaccinatiegraad per gemeente</h1>
         <Towns />
       </div>
   );
