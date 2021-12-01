e64b1d0ab8fd10976e60cbfd97de293b6c12c762
Jody Lorist <66092262+jody29@users.noreply.github.com>
2021-12-01 23:20:45 +0100

    bugfix

--- src/components/progres.js
+++ src/components/progres.js
@@ -1,4 +1,4 @@
-import { useContext } from "react/cjs/react.development"
+import { useContext } from "react"
 import { AgeContext } from "./provider"
 import color from "../helpers/checkPercentage.js";
 import colors from "./map";
