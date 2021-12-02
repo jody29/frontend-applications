2b0244ade21e84f0ceb12ae7ac38b47eb96c3d84
Jody Lorist <66092262+jody29@users.noreply.github.com>
2021-12-02 15:01:35 +0100

    REFACTOR: consistent code

--- src/components/provider.js
+++ src/components/provider.js
@@ -3,7 +3,7 @@ import './filter.css'
 
 export const AgeContext = createContext() // create context 
 
-export function Provider({ children }) { // create provider component
+export const Provider = ({ children }) => { // create provider component
     const [selectedAge, setAge] = useState('18+') // create context for the selectedAge with 18+ as default value
     const [selectedTown, setTown] = useState(null) // create context for the selectedTown with null as default value
     const [percentage, setPercentage] = useState(null) // create the percentage with null as default value
